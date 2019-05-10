60352866d18a44ca7ba76e7fdf0aa75b61af7c1e
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-10 22:25:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,42 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition	  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand	  attr_reader :brand
+
+   BRANDS = []
+
+
+   def initialize(brand)	  def initialize(brand)
+    @brand = brand	    @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
+  end	  end
+
+
+
+
+   def cobble	  def cobble
+    self.condition = "new"	    self.condition = "new"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Shoe
 
 def initialize(brand)
   @brand = brand 
