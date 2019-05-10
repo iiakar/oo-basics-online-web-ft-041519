8359bcdb2c34d0df416afe672404f47046987830
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-10 22:26:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,7 +9,9 @@ class Shoe
     @brand = brand
     BRANDS << brand unless 
     BRANDS.any? { |b| b == brand }
-  end	  end
+  end
+  
+end 
 
 
 
@@ -37,59 +39,3 @@ class Shoe
 
 
 
-class Shoe
-
-def initialize(brand)
-  @brand = brand 
-end
-
-def brand 
- @brand
-end 
-
-def properties=(tattered)
-  @tattered = tattered
-end
-
-def properties
-  @tattered
-end
-
-def condition=(tattered)
-  @tattered=tattered
-  
-  def condition 
-    @tattered 
-
-
-def color=(color)
-  @color = color
-end
-
-def color  
-  @color
-end
-
-def size=(num)
-  @size = num
-end 
-
-def size 
-  @size 
-end 
-
-def material=(material)
-  @material = material
-end
-
-def material 
-  @material 
-end 
-
-def cobble 
-  puts "The shoe has been repaired"
-
-end
-
-
-end
